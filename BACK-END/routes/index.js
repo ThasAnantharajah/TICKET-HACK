908f58af0b02e0a62c7a466a8c097caa1c2bb34b
mindangel77 <118635964+mindangel77@users.noreply.github.com>
2024-05-16 19:14:00 +0200

    TICKET-HACK modif index.js

--- BACK-END/routes/index.js
+++ BACK-END/routes/index.js
@@ -13,11 +13,11 @@ const moment = require('moment');
 });*/
 //2024-05-15T21:39:22.594+00:00
 router.get('/trips', (req, res) => {
-  const date = new Date (req.body.date);
+  const date = new Date (req.query.date);
   const datePlusOne = new Date (moment(date).add(1, "days"));
   console.log(date,datePlusOne );
   
-   Trip.find({departure: req.body.departure, arrival: req.body.arrival, date: { $gte: date, $lt: datePlusOne }}).then(data => {
+   Trip.find({departure: req.query.departure, arrival: req.query.arrival, date: { $gte: date, $lt: datePlusOne }}).then(data => {
    res.json({ trips:  data });
   })
   
