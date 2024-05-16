d240e3853cfcc038a4312f2deea7d7024c29f768
mindangel77 <118635964+mindangel77@users.noreply.github.com>
2024-05-16 11:36:22 +0200

    TICKET-HACK first version project

---
+++ FRONT-END/script.js
@@ -0,0 +1,13 @@
+console.log("bye")
+
+
+
+console.log(document.querySelector('.date-picker').value)
+
+const searcheBtn = document.querySelector('.searcheBtn')
+
+
+
+searcheBtn.addEventListener('click', function() {
+    console.log(document.querySelector('.date-picker').value)
+})
