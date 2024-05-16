5e8679fe4565dabf24415b299b416fea7d4f8c01
mindangel77 <118635964+mindangel77@users.noreply.github.com>
2024-05-16 11:40:03 +0200

    TICKET-HACK routes GET

--- BACK-END/routes/index.js
+++ BACK-END/routes/index.js
@@ -24,15 +24,6 @@ router.get('/trips', (req, res) => {
 });
 
 /*router.post('/trips', (req, res) => {
-  const newTrip = {
-    departure : req.body.departure,
-    arrival : req.body.arrival,
-    date : req.body.Date,
-    price: req.body.price,
-  }
-  Trip.push(newTrip);
-  //console.log(newtrip);
-  res.json({ trips: newTrip });
- });
+  });
 */
 module.exports = router;
